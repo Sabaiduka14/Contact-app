9ff0c68844ab936aaf83090a9e201b5da124a830
Sabaiduka14 <sabaidukashvili50@gmail.com>
2024-03-16 16:15:50 +0400

    new files and folders

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { addDoc, collection, getDocs, getFirestore, deleteDoc, doc, updateDoc } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
+  apiKey: "AIzaSyAPOrwM9buo1tSN2APvNRpFiyhZ3t0K3_k",
   authDomain: "contact-app1-ef43c.firebaseapp.com",
   projectId: "contact-app1-ef43c",
   storageBucket: "contact-app1-ef43c.appspot.com",
-  messagingSenderId: import.meta.VITE_SENDER_ID,
-  appId: import.meta.VITE_APP_ID,
+  messagingSenderId: "538487880733",
+  appId: "1:538487880733:web:3ce9ea698030308fc0daf4",
 };
 
 const app = initializeApp(firebaseConfig);
